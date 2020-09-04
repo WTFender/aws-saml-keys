d1385025301c3fcadf06c48360001d43b4a38ea4
WTFender <wtfender.cs@gmail.com>
2020-09-04 15:28:47 -0700

    bump version

--- build.ps1
+++ build.ps1
@@ -4,6 +4,6 @@ $env:GOARCH="amd64"; $env:GOOS="windows"; go build -o ./bin/ask_"$version".exe .
 $env:GOARCH="amd64"; $env:GOOS="darwin"; go build -o ./bin/ask_"$version"_mac ./cmd/ask/main.go
 $env:GOARCH="amd64"; $env:GOOS="linux"; go build -o ./bin/ask_"$version"_nix ./cmd/ask/main.go
 
-$env:GOARCH="amd64"; $env:GOOS="windows"; go build -o ./bin/installer.exe ./cmd/installer/main.go
-$env:GOARCH="amd64"; $env:GOOS="darwin"; go build -o ./bin/installer_mac ./cmd/installer/main.go
-$env:GOARCH="amd64"; $env:GOOS="linux"; go build -o ./bin/installer_nix ./cmd/installer/main.go
+#$env:GOARCH="amd64"; $env:GOOS="windows"; go build -o ./bin/installer.exe ./cmd/installer/main.go
+#$env:GOARCH="amd64"; $env:GOOS="darwin"; go build -o ./bin/installer_mac ./cmd/installer/main.go
+#$env:GOARCH="amd64"; $env:GOOS="linux"; go build -o ./bin/installer_nix ./cmd/installer/main.go
--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ GOARCH="amd64"; GOOS="windows"; go build -o ./bin/ask_"${version}".exe ./cmd/ask
 GOARCH="amd64"; GOOS="darwin"; go build -o ./bin/ask_"${version}"_mac ./cmd/ask/main.go
 GOARCH="amd64"; GOOS="linux"; go build -o ./bin/ask_"${version}"_nix ./cmd/ask/main.go
 
-GOARCH="amd64"; GOOS="windows"; go build -o ./bin/installer.exe ./cmd/installer/main.go
-GOARCH="amd64"; GOOS="darwin"; go build -o ./bin/installer_mac ./cmd/installer/main.go
-GOARCH="amd64"; GOOS="linux"; go build -o ./bin/installer_nix ./cmd/installer/main.go
+#GOARCH="amd64"; GOOS="windows"; go build -o ./bin/installer.exe ./cmd/installer/main.go
+#GOARCH="amd64"; GOOS="darwin"; go build -o ./bin/installer_mac ./cmd/installer/main.go
+#GOARCH="amd64"; GOOS="linux"; go build -o ./bin/installer_nix ./cmd/installer/main.go
